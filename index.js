c1c771b40cedf104758092a1dc48240f3f12a6c3
ferheen4535 <ferheen.syed@gmail.com>
2025-03-04 11:27:48 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+//Use arrays to store ordered lists of data.
+//Use objects to store keyed lists of data.
+//Use conditional logic to process data.
+//Use loops to handle repetitive tasks.
+//Transform data according to specifications.
